aaea926fdd301bb9c1529459c7107f3a3b56b16a
Kronos5675 <120796382+Kronos5675@users.noreply.github.com>
2025-03-06 11:27:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ def div(a,b):
     return(b/a)
     
 i=0
-while(i!=5):
+while(i!='5'):
     print("Hello, Welcome to Calculator")
     print("What do you want to do?")
     print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Exit")
-    i=int(input("Enter your Choice {1|2|3|4|5} : "))
-    if(i==1):
+    i=input("Enter your Choice {+|-|x|/|5} : ")
+    if(i=='+'):
         a = int(input('Enter 1st number: '))
         b = int(input('Enter 2nd number: '))
         print(f'Sum of {a} and {b} is {sum(a, b)}')
-    elif(i==2):
+    elif(i=='-'):
         a = int(input('Enter 1st number: '))
         b = int(input('Enter 2nd number: '))
         print(f'Difference of {a} from {b} is {sub(a, b)}')
-    elif(i==3):
+    elif(i=='x'):
         a = int(input('Enter 1st number: '))
         b = int(input('Enter 2nd number: '))
         print(f'Multiplication of {a} and {b} is {mul(a, b)}')
-    elif(i==4):
+    elif(i=='/'):
         a = int(input('Enter 1st number: '))
         b = int(input('Enter 2nd number: '))
         print(f'Division of {b} by {a} is {div(a, b)}')
-    elif(i==5):
+    elif(i=='5'):
         print("Thank You for using Calculator")
         quit()
     else:
